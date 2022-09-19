c40b3823561834da90cc94d1fb931cc2876d8d65
Icheka Ozuru <rhemafortune@gmail.com>
2022-09-19 23:21:36 +0100

    Add code-editor to tests in CI workflow.

--- devops/tests.sh
+++ devops/tests.sh
@@ -2,8 +2,21 @@
 
 . "$PWD"/devops/utils.sh
 
-# test sonar-v2
+# test sonar-lang
 cd sonar-lang || exit 1
 go test -v ./...
+test_exit_code
+cd ..
+
+# test language-server
+cd language-server || exit 1
+go test -v ./...
+test_exit_code
+cd ..
+
+# test code-editor
+cd code-editor || exit 1
+export CI=true
+npm run test
 test_exit_code
 cd ..
